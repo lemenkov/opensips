a56c81d11fcc3685d13f078beb6bf277151a2d4b
Peter Lemenkov <lemenkov@gmail.com>
2011-08-11 16:37:04 +0400

    Consistently use rtpproxy switches
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- modules/rtpproxy/rtpproxy.c
+++ modules/rtpproxy/rtpproxy.c
@@ -3384,8 +3384,6 @@ int force_rtp_proxy_body(struct sip_msg* msg, struct force_rtpp_args *args,
 			}
 			break;
 
-		case 'w':
-		case 'W':
 		case 's':
 		case 'S':
 			if (append_opts(&opts, 'S') == -1) {
